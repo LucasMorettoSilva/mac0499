43635038058969aec0785f9ed28bfbfb1cc9a1e6
Lucas Moretto da Silva <lucas.da.silva1@dell.com>
2022-08-16 18:47:02 -0300

    fix sdb script

--- pingr/db/user/create-tables.sql
+++ pingr/db/user/create-tables.sql
@@ -5,16 +5,16 @@ create table users(
     creation_date timestamp default now()   
 );
 
-insert into users (email, first_name, last_name)
-    values
-        ('user1@email.com', "user1", "user 1 last name"),
-        ('user2@email.com', "user2", "user 2 last name"),
-        ('user3@email.com', "user3", "user 3 last name");
-
-create table friendship(
+create table friendships(
     user_email_1 varchar(255) not null,
     user_email_2 varchar(255) not null,
     primary key (user_email_1, user_email_2),
     constraint fk_user_email_1 foreign key (user_email_1) references users(email),
     constraint fk_user_email_2 foreign key (user_email_2) references users(email)
 );
+
+insert into users (email, first_name, last_name)
+    values
+        ('user1@email.com', 'user1', 'user 1 last name'),
+        ('user2@email.com', 'user2', 'user 2 last name'),
+        ('user3@email.com', 'user3', 'user 3 last name');
