efc2fad7eceba38a13e38431dda93f3217798ea9
Lucas Moretto da Silva <lucas.da.silva1@dell.com>
2022-08-18 17:42:35 -0300

    add endpoint to check for friendship existence

--- pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/controller/FriendshipController.kt
+++ pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/controller/FriendshipController.kt
@@ -3,6 +3,8 @@ package br.usp.syncmsconnection.controller
 import br.usp.syncmsconnection.model.entity.Friendship
 import br.usp.syncmsconnection.service.friendship.EstablishFriendshipService
 import br.usp.syncmsconnection.service.friendship.FriendshipService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -10,12 +12,23 @@ import javax.validation.Valid
 @RequestMapping("/api/friendship")
 class FriendshipController(
     private val friendshipService: FriendshipService,
-    private val establishFriendshipService: EstablishFriendshipService) {
+    private val establishFriendshipService: EstablishFriendshipService
+) {
 
     @GetMapping
     fun findAll() = friendshipService.findAll()
 
+    @GetMapping("{email1}/user/{email2}")
+    fun find(@PathVariable email1: String, @PathVariable email2: String): ResponseEntity<Friendship> {
+        val friendship =
+            friendshipService.find(email1, email2) ?: return ResponseEntity.notFound().build()
+
+        return ResponseEntity.ok(friendship)
+    }
+
     @PostMapping
     fun createFriendship(@Valid @RequestBody friendship: Friendship) =
-        establishFriendshipService.createFriendship(friendship)
+        ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(establishFriendshipService.createFriendship(friendship))
 }
--- pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/service/friendship/FriendshipService.kt
+++ pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/service/friendship/FriendshipService.kt
@@ -1,13 +1,14 @@
 package br.usp.syncmsconnection.service.friendship
 
 import br.usp.syncmsconnection.model.entity.Friendship
-import org.springframework.http.ResponseEntity
 
 interface FriendshipService {
 
-    fun save(friendship: Friendship): ResponseEntity<Friendship>
+    fun save(friendship: Friendship): Friendship
 
     fun exists(friendship: Friendship): Boolean
 
     fun findAll(): List<Friendship>
+
+    fun find(userEmail1: String, userEmail2: String): Friendship?
 }
