d8e1075323e630b262b13aaba4253d79d1ddaa9f
Lucas Moretto da Silva <lucas.da.silva1@dell.com>
2022-08-22 10:32:00 -0300

    add logs to sync-ms-connection

--- pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/service/friendship/EstablishFriendshipServiceImpl.kt
+++ pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/service/friendship/EstablishFriendshipServiceImpl.kt
@@ -2,6 +2,9 @@ package br.usp.syncmsconnection.service.friendship
 
 import br.usp.syncmsconnection.model.entity.Friendship
 import br.usp.syncmsconnection.service.user.UserService
+import jdk.nashorn.internal.runtime.regexp.joni.Config.log
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
@@ -11,7 +14,11 @@ class EstablishFriendshipServiceImpl(
     private val friendshipService: FriendshipService,
     private val userService: UserService): EstablishFriendshipService {
 
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+
     override fun createFriendship(friendship: Friendship) {
+        log.info("createFriendship() : preparing to create new friendship : {}", friendship)
+
         validateUserExistence(friendship.userEmail1)
         validateUserExistence(friendship.userEmail2)
 
--- pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/service/friendship/FriendshipServiceImpl.kt
+++ pingr/sync/sync-ms-connection/src/main/kotlin/br/usp/syncmsconnection/service/friendship/FriendshipServiceImpl.kt
@@ -3,6 +3,8 @@ package br.usp.syncmsconnection.service.friendship
 import br.usp.syncmsconnection.model.entity.Friendship
 import br.usp.syncmsconnection.model.entity.FriendshipId
 import br.usp.syncmsconnection.repository.FriendshipRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -12,13 +14,32 @@ import org.springframework.web.server.ResponseStatusException
 class FriendshipServiceImpl(
     private val friendshipRepo: FriendshipRepository): FriendshipService {
 
-    override fun findAll(): List<Friendship> = friendshipRepo.findAll()
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
 
-    override fun save(friendship: Friendship): Friendship =
-        friendshipRepo.save(friendship)
+    override fun findAll(): List<Friendship> {
+        log.info("findAll() : searching for all friendships in db")
+
+        val friendships = friendshipRepo.findAll()
+
+        log.info("findAll() : total found : {}", friendships.size)
+
+        return friendships
+    }
+
+    override fun save(friendship: Friendship): Friendship {
+        log.info("save() : updating friendship : {}", friendship)
+
+        val entity = friendshipRepo.save(friendship)
+
+        log.info("save() : friendship successfully updated : {}", entity)
+
+        return entity
+    }
 
     override fun exists(friendship: Friendship): Boolean {
-        return friendshipRepo.existsById(
+        log.info("exists() : checking existence of friendship : {}", friendship)
+
+        val friendshipExists = friendshipRepo.existsById(
             FriendshipId(
                 friendship.userEmail1,
                 friendship.userEmail2
@@ -29,22 +50,40 @@ class FriendshipServiceImpl(
                 friendship.userEmail1
             )
         )
+
+        log.info("exists() : friendship exists : {}", friendshipExists)
+
+        return friendshipExists
     }
 
     override fun find(userEmail1: String, userEmail2: String): Friendship? {
-        val friendship = friendshipRepo.findById(
-            FriendshipId(userEmail1, userEmail2)
-        )
+        val friendshipId = FriendshipId(userEmail1, userEmail2)
 
-        if (friendship.isPresent) {
-            return friendship.get()
+        log.info("find() : searching for friendship : {}", friendshipId)
+
+        var friendship = friendshipRepo.findByIdOrNull(friendshipId)
+
+        if (friendship != null) {
+            log.info("find() : friendship successfully found")
+
+            return friendship
         }
 
-        return friendshipRepo.findByIdOrNull(
+        friendship = friendshipRepo.findByIdOrNull(
             FriendshipId(userEmail2, userEmail1)
-        ) ?: throw ResponseStatusException(
-            HttpStatus.NOT_FOUND,
-            "friendship between users not found"
         )
+
+        if (friendship == null) {
+            log.info("find() : friendship not found")
+
+            throw ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "friendship between users not found"
+            )
+        }
+
+        log.info("find() : friendship successfully found")
+
+        return friendship
     }
 }
