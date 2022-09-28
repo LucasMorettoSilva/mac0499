4181da6462b1735b31b6ae2c20b74850b94cedc7
Lucas Moretto da Silva <lucas.da.silva1@dell.com>
2022-09-28 16:39:57 -0300

    add merge

--- pingr/scripts/plot.py
+++ pingr/scripts/plot.py
@@ -2,22 +2,58 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def main():
-    df_sync = pd.read_csv("./measures-sync.csv")
-    df_sync_docker = pd.read_csv("./measures-sync-docker.csv")
+class MeasureDataFrames:
 
-    axes = df_sync.plot(kind='line', y='elapsed_time', label='local')
-    df_sync_docker.plot(
-        kind='line',
-        y='elapsed_time',
-        ax=axes,
-        label='docker',
-        title='Total request time to complete',
-        ylabel='total time (ms)',
-    )
+    def __init__(self):
+        self.df_sync = pd.read_csv("./measures-sync.csv")
+        self.df_async = pd.read_csv("./measures-async.csv")
+        self.df_sync_docker = pd.read_csv("./measures-sync-docker.csv")
+        self.df_async_docker = pd.read_csv("./measures-async-docker.csv")
 
-    plt.show()
+    def plot(self):
+        axes = plt.gca()
+
+        self.plot_sync(axes)
+        self.plot_async(axes)
+
+    def plot_sync(self, axes=plt.gca()):
+        self.df_sync.plot(
+            kind='line',
+            y='elapsed_time',
+            label='sync-local',
+            ax=axes
+        )
+
+        self.df_sync_docker.plot(
+            kind='line',
+            y='elapsed_time',
+            ax=axes,
+            label='sync-docker',
+            title='Total request time to complete',
+            ylabel='total time (ms)',
+        )
+
+    def plot_async(self, axes=plt.gca()):
+        self.df_async.plot(
+            kind='line',
+            y='elapsed_time',
+            label='async-local',
+            ax=axes
+        )
+
+        self.df_async_docker.plot(
+            kind='line',
+            y='elapsed_time',
+            ax=axes,
+            label='async-docker',
+            title='Total request time to complete',
+            ylabel='total time (ms)',
+        )
 
 
 if __name__ == "__main__":
-    main()
+    df = MeasureDataFrames()
+
+    df.plot_async()
+
+    plt.show()
